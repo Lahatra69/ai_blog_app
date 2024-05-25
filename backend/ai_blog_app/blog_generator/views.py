ed7b0cb9bdc73f730ca4194f840183b36c3f44f7
Lahatra69 <andrianjazalahatramendrika@gmail.com>
2024-05-25 22:10:46 +0300

    add login and logout method

--- backend/ai_blog_app/blog_generator/views.py
+++ backend/ai_blog_app/blog_generator/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, "index.html")
 
 
 def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
+            return render(
+                request, "login.html", {"error_mesage": "Invalid username or password"}
+            )
     return render(request, "login.html")
 
 
@@ -38,4 +52,5 @@ def user_signup(request):
 
 
 def user_logout(request):
-    pass
+    logout(request)
+    return redirect("/")
